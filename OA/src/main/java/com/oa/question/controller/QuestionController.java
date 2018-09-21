bffd8dd53a2ba6c2addecd0e840a1bbee90d66d2
Administrator <Administrator@WIN-6SBV0QG4LEF>
2018-09-21 10:22:33 +0800

    hxd wudi

---
+++ OA/src/main/java/com/oa/question/controller/QuestionController.java
@@ -0,0 +1,11 @@
+package com.oa.question.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+}
