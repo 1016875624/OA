9021ed0acb0ab906ba0e11b2a1fb8d85bbb8f17a
neal <499859073@qq.com>
2018-10-15 09:02:58 +0800

    webOV0.1

---
+++ OA/admin-dashboard/app/model/officeResource/OfficeResourceModel.js
@@ -0,0 +1,23 @@
+﻿Ext.define('Admin.model.officeResource.OfficeResourceModel', {
+	extend: 'Admin.model.Base',
+	requires: [
+		'Ext.data.proxy.Rest'
+	],
+	fields: [
+		{type:'int',name:'id'}
+		,{type:'string',name:'employeeId'}
+		,{type:'string',name:'employeeName'}
+		,{type:'string',name:'employeePosition'}
+		,{type:'date',name:'startTime',dateFormat:'Y/m/d H:i:s'}
+		,{type:'date',name:'endTime',dateFormat:'Y/m/d H:i:s'}
+		,{type:'date',name:'applyTime',dateFormat:'Y/m/d H:i:s'}
+		,{type:'int',name:'leftCount'}
+		,{type:'string',name:'resourceName'}
+		,{type:'int',name:'remark'}
+		,{type:'int',name:'status'}
+	],
+	proxy: {
+		type: 'rest',
+		url: '/officeResource',
+	}
+});
