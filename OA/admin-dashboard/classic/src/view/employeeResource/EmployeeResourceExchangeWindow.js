49aa7035a6f815b58babf3dafc0e9f7ab8170885
neal <499859073@qq.com>
2018-10-23 11:33:35 +0800

    add some function

---
+++ OA/admin-dashboard/classic/src/view/employeeResource/EmployeeResourceExchangeWindow.js
@@ -0,0 +1,55 @@
+﻿Ext.define('Admin.view.employeeResource.EmployeeResourceExchangeWindow', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.employeeResourceExchangeWindow',
+	autoShow: true,
+    height: 350,
+    minHeight: 350,
+    minWidth: 300,
+    width: 500,
+    scrollable: true,
+    title: 'Exchange EmployeeResource Window',
+    closable: true,
+    constrain: true,
+    defaultFocus: 'textfield',
+    modal:true,
+    layout: 'fit',
+	items: [{
+        xtype: 'form',
+        layout: 'form',
+        padding: '10px',
+        ariaLabel: 'Enter your name',
+        items: [{
+            xtype: 'textfield',
+            fieldLabel: 'id',
+            name:'id',
+            hidden: true,
+            readOnly: true
+        },{
+            xtype: 'textfield',
+            fieldLabel: '资源名称',
+            name:'resourceName',
+			readOnly: true
+        },{
+            xtype: 'textfield',
+            fieldLabel: '原有数目',
+            name:'count',
+			readOnly: true
+        },{
+            xtype: 'textfield',
+            fieldLabel: '兑换数目',
+            name:'exchangeCount'
+        }]
+    }],
+   buttons: ['->',{
+        xtype: 'button',
+        text: 'Submit',
+        handler: 'submitExchangeForm'
+    },{
+        xtype: 'button',
+        text: 'Close',
+        handler: function(btn) {
+            btn.up('window').close();
+        }
+    },'->']
+  
+});
--- OA/admin-dashboard/classic/src/view/employeeResource/EmployeeResourceViewController.js
+++ OA/admin-dashboard/classic/src/view/employeeResource/EmployeeResourceViewController.js
@@ -5,6 +5,19 @@
 	openSearchWindow:function(toolbar, rowIndex, colIndex){
 		toolbar.up('panel').up('container').add(Ext.widget('employeeResourceSearchWindow')).show();
 	},
+	/*Exchange More*/
+	openExchangeWindow:function(grid, rowIndex, colIndex){
+		var record = grid.getStore().getAt(rowIndex);
+		if (record) {
+			if(record.data.status=="1"){
+				var win = grid.up('container').add(Ext.widget('employeeResourceExchangeWindow'));
+				win.show();
+				win.down('form').getForm().loadRecord(record);
+			}else{
+				Ext.Msg.alert('提示', "只可以兑换'已拥有'状态的信息！");
+			}
+		}
+	},
 	/*combobox选中后控制对应输入（文本框和日期框）框显示隐藏*/
 	searchComboboxSelectChuang:function(combo,record,index){
 		//alert(record.data.name);
@@ -62,6 +75,38 @@
 		});
 		store.load({params:{start:0, limit:20, page:1}});
 		win.close();
+	},
+	/*Exchange Submit*/	
+	submitExchangeForm:function(btn){
+		var win    = btn.up('window');
+		var values  = win.down('form').getValues();//获取form数据
+		if(values.exchangeCount==''||values.exchangeCount==undefined||values.exchangeCount==null){
+			alert('兑换数目不能为空');
+		}
+		else if(values.count<values.exchangeCount){
+			alert('兑换数目不能多于你已拥有的数目');
+		}
+		else{
+			Ext.Ajax.request({
+				url : '/employeeResource/exchangeResource', 
+				method : 'post',
+				params : {
+					id :values.id,
+					exchangeCount :values.exchangeCount
+				}, 
+				success: function(response, options) {
+					var json = Ext.util.JSON.decode(response.responseText);
+					if(json.success){
+						Ext.Msg.alert('操作成功', json.msg, function() {
+						Ext.data.StoreManager.lookup('employeeResourceStore').load();
+						win.close();
+					});
+					}else{
+						Ext.Msg.alert('操作失败', json.msg);
+					}
+				}
+			});
+		}
 	},
 	sureTrade:function(grid, rowIndex, colIndex){
 		var record = grid.getStore().getAt(rowIndex);
